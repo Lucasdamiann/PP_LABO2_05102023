30ac8c62890d8feb658c74ea76bc40a2af72aa6e
Lucas Bracuto <lucasdamiann@gmail.com>
2023-10-05 22:15:17 -0300

    segundo commit parcial

--- Entidades/Entidades/SistemaBinario.cs
+++ Entidades/Entidades/SistemaBinario.cs
@@ -11,5 +11,43 @@ namespace Entidades
         public SistemaBinario(string valor): base(valor)
         {
         }
+
+        internal override double ValorNumerico
+        {
+            get
+            {
+                return (double)decimal.Parse(this.Valor); ;
+            }
+        }
+
+        public override Numeracion CambiarSistemaDeNumeracion(ESistema sistema)
+        {
+            SistemaDecimal numeracion;
+            if (sistema == ESistema.Decimal)
+            {
+                numeracion = new SistemaDecimal(decimal.Parse(this.Valor).ToString());
+                return numeracion;
+            }
+            else
+            {
+                return DecimalABinario();
+            }
+        }
+        private SistemaBinario DecimalABinario()
+        {
+            string binario = "";
+            if (int.TryParse(this.valor, out int numero))
+            {
+                while (numero > 0)
+                {
+                    int residuo = numero % 2;
+                    binario += residuo;
+                    numero /= 2;
+                }
+            }
+            return new SistemaBinario(binario);
+        }
+
+
     }
 }
